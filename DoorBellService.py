dc5acb82582ecd41e73d2512c300d22a03e9323a
andyseubert <andys@florapdx.com>
2014-09-08 15:42:51 -0700

    adding some logging to the client

--- DoorBellService.py
+++ DoorBellService.py
@@ -2,18 +2,31 @@ import socket
 import sys
 from thread import *
 import winsound
+import win32evtlogutil
+import win32api
+import win32con
+import win32security # To translate NT Sids to account names.
+import win32evtlog
 
+
+logType = "Application"
+ApplicationName="DoorBellService.py"
+win32evtlogutil.ReportEvent(logType, 0,strings=['DOORBELL SERVICE READY'],
+                            eventType=win32evtlog.EVENTLOG_INFORMATION_TYPE,
+                            data = "Raw\0Data".encode("ascii"))
+        
 def DoorBellService():
     HOST = ''   # Symbolic name meaning all available interfaces
     PORT = 9999
-    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #print 'Socket created'
     
     try:
         s.bind((HOST, PORT))
     except socket.error , msg:
-        #print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        win32evtlogutil.ReportEvent(logType, 2,strings=['Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1],"ERROR DOORBELL"],
+                                    data = "Raw\0Data".encode("ascii"))
         sys.exit()
     
     #print 'Socket bind complete'
